5e8e993097928dee5a55c659ed26c2c6900504c6
ragu-S <ragu.sivanandha@gmail.com>
2017-06-10 20:28:05 -0400

    updating with more notes

---
+++ Notes/all-drawable-tool.js
@@ -0,0 +1,19 @@
+// All drawable/interactable elements
+Line
+  -> Bordered/Boxed/Dashed
+  -> Emphasized (ie. bolded or bigger font)
+  -> colored
+
+GroupedLine
+  -> Bordered/Boxed/Dashed
+  -> Emphasized (ie. bolded or bigger font)
+  -> colored
+
+Graph
+
+Chart
+
+Illustration
+Image
+Video
+
---
+++ Notes/draw-tool.js
@@ -0,0 +1,17 @@
+// Tools that listen to user interaction events (pointer events)
+
+1) draw
+// mouse/pointer events for Draw
+down()
+  isDown = true
+  // create Graphics and store in class
+  // store (x,y) in path var
+up()
+  isDown = false
+  // clear path var
+  // Convert to cached bitMap or image
+  // delay 1 sec then call character recognition on canvas image
+move()
+  // Store (x,y) in path var
+  // draw Polygon using path
+
